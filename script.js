884f82cb431d6f1abf140e372cbc5e5ddbadc60a
tanudawande04 <tanudawande54@gmail.com>
2025-07-02 00:00:04 +0530

    added methods

---
+++ script.js
@@ -0,0 +1,13 @@
+Document.addEventListener(Domcontentloaded => () {
+    const products = [
+        { id: 1, name: "product1", price: 100.0 },
+        { id: 2, name: "product2", price: 100.0 },
+        { id: 3, name: "product3", price: 100.0 },
+
+    ];
+
+    constcart = [];
+    const cartitems = document.getElementsByClassName("cart-items");
+
+
+})
