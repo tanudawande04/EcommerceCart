8b858154b1ef869606eece57d74e90c238343d33
tanudawande04 <tanudawande54@gmail.com>
2025-07-04 12:29:57 +0530

    added new functionality

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const cart = [];
     const productList = document.getElementById("product-list");
-    const emptyCart = document.getElementById("empty-cart");
-    const cartTotal = document.getElementById("cart-total");
-    const itemprice = document.getElementById("total-price");
-    const checkoutBtn = document.getElementById("chekout-btn");
+    const cartItems = document.getElementById("cart-items");
+    const emptyCartMessage = document.getElementById("empty-cart");
+    const cartTotalMessage = document.getElementById("cart-total");
+    const totalPriceDisplay = document.getElementById("total-price");
+    const checkOutBtn = document.getElementById("chekout-btn");
 
     products.forEach(product => {
         const productDiv = document.createElement('div');
@@ -39,21 +40,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function addToCart(product) {
         cart.push(product);
-
+        renderCart();
     }
 
     function renderCart() {
-        cartItems.innerHTML = "";
+        cartItems.innerText = "";
         let totalPrice = 0;
 
         if (cart.length > 0) {
-            emptyCartMessage.classList.add();
+            emptyCartMessage.classList.add('hidden');
             cartTotalMessage.classList.remove("hidden")
+
+            cart.forEach((item, index) => {
+                totalPrice += item.price;
+                const cartItem = document.createElement('div');
+
+                cartItem.innerHTML = `
+                ${item.name} - $${item.price.toFixed(2)}
+
+                `;
+                cartItems.appendChild(cartItem)
+                totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
+
+
+            });
+
 
         } else {
             emptyCartMessage.classList.remove("hidden");
+            totalPriceDisplay.textContent = `$0.00`;
 
         }
     }
+
+    checkOutBtn.addEventListener("click", () => {
+        cart.length = 0;
+        alert("chekout successfully");
+        renderCart();
+
+    })
+
+
 });
 
